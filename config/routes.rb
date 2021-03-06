d7868c8ed5405c5e33ffa7349c96b2859d9df6fb
diamond-nicholas <diamondnicholas154@gmail.com>
2021-03-06 22:42:32 +0100

    fix login for user and generate controller dashboard for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
   resources :users
-  devise_for :accounts
+  devise_for :accounts, :controllers => { registrations: 'registrations'}
+
   root to: 'crypto#main'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
